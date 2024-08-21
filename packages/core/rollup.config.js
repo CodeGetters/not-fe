50788e0966b2590c52aceac309169fb68dc12160
yuanxin06 <yuanxin06@corp.netease.com>
2024-08-21 21:52:04 +0800

    build: 更新rollup config

---
+++ packages/core/rollup.config.js
@@ -0,0 +1,40 @@
+import typescript from "@rollup/plugin-typescript";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import babel from "rollup-plugin-babel";
+import clear from "rollup-plugin-clear";
+import { terser } from "rollup-plugin-terser";
+
+export default {
+  input: "src/index.ts",
+  /*构建两套，一个 umd 通用的，一个 ES 模块的，webpack 等可以直接用 ES 的，便于代码提示*/
+  output: [
+    {
+      // dir: "./dist/umd/",
+      file: "dist/index.js",
+      format: "umd",
+      name: "index",
+      sourcemap: true,
+    },
+    {
+      // dir: "dist/esm",
+      file: "dist/index.esm.js",
+      format: "es",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    /*打包之前清除 dist 目录*/
+    clear({ target: ["dist"] }),
+    resolve(),
+    commonjs(),
+    /*压缩代码*/
+    terser(),
+    /*babel 处理*/
+    babel({
+      exclude: "node_modules/**",
+      runtimeHelpers: true,
+    }),
+    typescript({ tsconfig: "./tsconfig.json" }),
+  ],
+};
