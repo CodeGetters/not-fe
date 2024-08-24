db14c26a636f26e2a38dbf1928e875edcee47818
yuanxin06 <yuanxin06@corp.netease.com>
2024-08-24 23:00:38 +0800

    feat: promise pending 状态解决，then方法链式调用部分

--- packages/core/src/Promise/index.ts
+++ packages/core/src/Promise/index.ts
@@ -22,12 +22,14 @@ class Promise {
       if (this.status === STATUS.PENDING) {
         this.status = STATUS.FULFILLED;
         this.value = value;
+        this.onResolveCall.forEach((fn) => fn());
       }
     };
     let reject = (reason) => {
       if (this.status === STATUS.REJECTED) {
         this.status = STATUS.REJECTED;
         this.reason = reason;
+        this.onRejectCall.forEach((fn) => fn());
       }
     };
     try {
@@ -36,21 +38,67 @@ class Promise {
       reject(error);
     }
   }
+  #resolveThen(newPromise, x, resolve, reject) {
+    if (newPromise === x)
+      return reject(new TypeError("This is a circular reference"));
+    let called: boolean;
+    if ((typeof x === "object" && x !== null) || typeof x === "function") {
+      try {
+        if (typeof x.then === "function") {
+        } else {
+          reject(x);
+        }
+      } catch (error) {
+        reject(error);
+      }
+    }
+  }
   then(onFulfilled: (value) => void, onRejected: (reason) => void) {
-    if (this.status === STATUS.FULFILLED) {
-      onFulfilled(this.value);
-    }
-    if (this.status === STATUS.REJECTED) {
-      onRejected(this.reason);
-    }
-    if (this.status === STATUS.PENDING) {
-      this.onResolveCall.push(() => {
-        onFulfilled(this.value);
-      });
-      this.onRejectCall.push(() => {
-        onRejected(this.reason);
-      });
-    }
+    // then 的参数可以缺省
+    onFulfilled =
+      typeof onFulfilled === "function" ? onFulfilled : (value) => value;
+    onRejected =
+      typeof onRejected === "function" ? onRejected : (reason) => reason;
+
+    // 每次调用 then 时，会创建一个新的 Promise 对象
+    const newPromise = new Promise((resolve, reject) => {
+      if (this.status === STATUS.FULFILLED) {
+        setTimeout(() => {
+          try {
+            // 获取返回值并调用方法根据返回值的类型调用进行处理
+            const x = onFulfilled(this.value);
+            this.#resolveThen(newPromise, x, resolve, reject);
+          } catch (error) {
+            reject(error);
+          }
+        }, 0);
+      }
+      if (this.status === STATUS.REJECTED) {
+        setTimeout(() => {
+          try {
+            // onRejected(this.reason);
+          } catch (error) {
+            reject(error);
+          }
+        }, 0);
+      }
+      if (this.status === STATUS.PENDING) {
+        this.onResolveCall.push(() => {
+          try {
+            // onFulfilled(this.value);
+          } catch (error) {
+            reject(error);
+          }
+        });
+        this.onRejectCall.push(() => {
+          try {
+            // onRejected(this.reason);
+          } catch (error) {
+            reject(error);
+          }
+        });
+      }
+    });
   }
   catch(reason) {}
 }
--- packages/playground/index.js
+++ packages/playground/index.js
@@ -1,17 +1,49 @@
-// import { Promise } from "@repo/core";
-const Promise = require("@repo/core").Promise;
+import { Promise } from "@repo/core";
+// let promise = new Promise((resolve, reject) => {
+//   console.log("promise resolve");
+//   // resolve("ok");
+// });
+
+// console.log("promise start");
+
+// promise.then(
+//   (value) => {
+//     console.log(value);
+//   },
+//   (error) => {
+//     console.log(error);
+//   },
+// );
+// (() => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("promise resolve");
+//     }, 1000);
+//     resolve("ok");
+//   }).then((res) => {
+//     console.log(res);
+//   });
+// })();
+
 let promise = new Promise((resolve, reject) => {
   console.log("promise resolve");
-  // resolve("ok");
+  setTimeout(() => {
+    resolve("ok");
+  }, 1000);
 });
 
 console.log("promise start");
 
-promise.then(
-  (value) => {
-    console.log(value);
-  },
-  (error) => {
-    console.log(error);
-  },
-);
+promise
+  .then(
+    (value) => {
+      console.log(value);
+      return "1";
+    },
+    (error) => {
+      console.log(error);
+    },
+  )
+  .then((res) => {
+    console.log("finally", res);
+  });
